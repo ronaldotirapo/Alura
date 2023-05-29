64c842f33242717e41d2b9f40e1708d6c38965dc
Rozt <rztirapo@gmail.com>
2023-05-29 14:51:55 -0500

    Agregado comentarios sobre instalación de NodeJS, hola mundo y declaración de variables

---
+++ Front End/JavaScript - primeros pasos con el lenguaje/01-HolaMundo.js
@@ -0,0 +1,5 @@
+/*Para verificar la version de NodeJS se utiliza el comando node -v
+Para operar NodeJS en una consola se utiliza el comando node
+Para actualiar NodeJS se puede descargar de su página o utilizar el comando npm install npm -g para instalar de forma global*/
+
+console.log("Hola mundo!");
---
+++ Front End/JavaScript - primeros pasos con el lenguaje/01-NombreVariables.js
@@ -0,0 +1,9 @@
+/*Existen distintas formas de declarar variables en JavaScript,
+aunque se puede declarar variables sin las palabras reservadas es mejor hacerlas según se necesiten:
+const, para constantes
+var, para variables con acceso global
+let, para variables con acceso local*/
+const nombre = "Ronaldo Tirapo";
+let direccion = "Av. Caraz 275";
+var costoPasaje = 1500;
+let tasaEmabrque = 50.5;
