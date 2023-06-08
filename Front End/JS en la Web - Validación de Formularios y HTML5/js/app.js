4f6dd59ec3d04745895ec1f353c2076336f7bc1c
Rozt <rztirapo@gmail.com>
2023-06-08 00:51:16 -0500

    Agregado funciones para evaluar los inputs mediante sus valores en el objeto validity y modificar el HTML dinámicamente para que muestre mensajes de error personalizados cuando el formato no es correcto al salir del foco

--- Front End/JS en la Web - Validación de Formularios y HTML5/js/app.js
+++ Front End/JS en la Web - Validación de Formularios y HTML5/js/app.js
@@ -1,11 +1,12 @@
-//Importamos nuestra función general valida para evaluar el tipo de input según el contenido de su data attribute
-import { valida } from "./validaciones";
+//Importamos nuestra función general valida para evaluar el tipo de input según el contenido de su data attribute tipo
+import { valida } from "./validaciones.js";
 
 //Guardamos todos los elementos input en el array que devolverá querySelectorAll
 const inputs = document.querySelectorAll("input");
 
 /*A cada elemento input le añadimos un listener para que cuando ocurra blur (se quita el foco del elemento)
-ejecute la función general valida(), la cual ejecutará una función según el contenido de su data attribute*/
+ejecute la función anónima que recibirá el evento y ejecutará el target del evento en la función general valida(), 
+(la cual ejecutará una función según el contenido de su data attribute tipo)*/
 inputs.forEach((input) => {
     input.addEventListener("blur", (event) => {
         valida(event.target);
--- Front End/JS en la Web - Validación de Formularios y HTML5/js/validaciones.js
+++ Front End/JS en la Web - Validación de Formularios y HTML5/js/validaciones.js
@@ -1,18 +1,73 @@
 //Para hacer funciones más reutilizables exportamos una función general que pueda ejecutar la validación según el tipo de input
 export function valida (input) {
-    //Con la propiedad dataSet accedemos al contenido de su data attribute con el nombre tipo
-    const tipoInput = input.dataSet.tipo;
+    //Con la propiedad dataset.tipo accedemos al contenido de su data attribute con el nombre tipo
+    const tipoInput = input.dataset.tipo;
     
     //Si existe el contenido del data attribute dentro del objeto validaciones ejecuta la función asociada
     if (validaciones[tipoInput]) {
         validaciones[tipoInput](input);
     }
+
+    /*Si la clave valid del objeto validity es true quitamos la clase input-container--invalid, sino la añadimos a su parent 
+    (ya que cuando input-message-error tiene de clase padre a input-container--invalid hará que aparezca el span) 
+    y establecemos el mensaje de error dentro del span de clase input-message-error*/
+    if (input.validity.valid) {
+        input.parentElement.classList.remove("input-container--invalid");
+        //Si no hay error no establecemos el mensaje de error que aparecerá en el span al agregar la clase input-message-error
+        input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    } else {
+        input.parentElement.classList.add("input-container--invalid");
+        //Si hay error establecemos el mensaje de error que aparecerá en el span al agregar la clase input-message-error
+        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoInput, input);
+    }
 }
 
-//Establecemos un objeto para guardar el par: contenido del data attribute y función para el input
+//Establecemos un objeto para guardar el par clave-valor como: contenido del data attribute y función para el input respectivamente
 const validaciones = {
-    //Utilizamos funciones anónimas de una sola linea para establecer la función para el input
-    "nacimiento" : (input) => validarNacimiento(input),
+    //Utilizamos funciones anónimas de una sola linea para establecer la función según el contenido del data attribute tipo
+    nacimiento : (input) => validarNacimiento(input),
+}
+
+//Creamos un array para recorrer el tipo de validity, así no tendremos que hacer recorridos anidados a mensajesDeError
+const tiposDeError = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError"
+]
+
+//Creamos un objeto anidado para guardar los distintos mensajes de error según los valores del objeto validity de nuestro input
+const mensajesDeError = {
+    nombre: {
+        valueMissing: "El campo nombre no debe estar vacío",
+    },
+    email: {
+        valueMissing: "El campo email no debe estar vacío",
+        typeMismatch: "Este correo no es válido",
+    },
+    password: {
+        valueMissing: "El campo password no debe estar vacío",
+        patternMismatch: "Al menos 6 carácters, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener carácteres especiales",
+    },
+    nacimiento: {
+        valueMissing: "El campo nacimiento no debe estar vacío",
+        //El valor customError se define cuando utilizamos setCustomValidity para validar un el input de un formulario
+        customError: "Debe ser mayor de 18 años"
+    }
+}
+
+/*Utilizamos una función que retorna un mensaje según el contenido de data attribute tipo del input
+y el tipo de error en su objeto validity */
+function mostrarMensajeDeError (tipoInput, input) {
+    let mensaje = "";
+    /*Recorremos cada tipo de error que tenemos y si está presente, es true,
+    en el objeto validity del input establecemos un mensaje según corresponda del objeto mensajesDeError*/
+    tiposDeError.forEach(error => {
+        if (input.validity[error]) {
+            mensaje = mensajesDeError[tipoInput][error];
+         }
+    });
+    return mensaje;
 }
 
 //La función extrae la fecha del input y si la validación de mayor de edad no es correcta se establece un mensaje
